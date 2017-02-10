5da494cc051d869ef07dfc261380867922a1deb6
Maciej Samoraj <maciej.samoraj@gmail.com>
2017-02-10 12:11:23 +0100

    replaced leaflet-map with plain leaflet

--- app/map.js
+++ app/map.js
@@ -1,8 +1,36 @@
+const Polymer = window.Polymer
+const L = window.L
 Polymer({
   is: 'vientos-map',
+  map: undefined,
+  markers: undefined,
+  ready () {
+    this.map = L.map(this.$.map)
+    this.icon = L.icon({ iconUrl: 'https://stendhalgame.org/images/mapmarker/me.png' })
+    this.map.setView([this.latitude, this.longitude], this.zoom)
+
+    L.tileLayer(this.tilelayer).addTo(this.map)
+    this.markers = L.layerGroup().addTo(this.map)
+    this.map
+      .locate()
+      .on('locationfound', e => {
+        this.myLatitude = e.latitude
+        this.myLongitude = e.longitude
+        this.myAccuracy = e.accuracy
+      })
+      .on('zoomend', e => {
+        this.zoom = this.map.getZoom()
+      })
+      .on('moveend', e => {
+        this.latitude = this.map.getCenter().lat
+        this.longitude = this.map.getCenter().lng
+      })
+    this._drawMarkers()
+  },
   properties: {
     locations: {
-      type: Array
+      type: Array,
+      observer: '_updatedLocations'
     },
     map: {
       type: Object
@@ -15,6 +43,7 @@ Polymer({
     },
     zoom: {
       type: Number
+
     },
     boundingBox: {
       type: Object,
@@ -35,12 +64,25 @@ Polymer({
       value: () => { return window.vientos.config.map.tilelayer }
     }
   },
-
+  _drawMarkers () {
+    this.markers.clearLayers()
+    this.locations.forEach(l => {
+      L.marker([l.latitude, l.longitude], { project_id: l.project._id, icon: this.icon })
+        .addTo(this.markers)
+        .on('click', e => {
+          this.fire('selected', e.target.options.project_id)
+        })
+    })
+  },
+  _updatedLocations () {
+    if (this.map) {
+      this._drawMarkers()
+    }
+  },
   _getBoundingBox () {
-    let map = this.$.map.map
-    if (map) {
+    if (this.map) {
       let sw, ne
-      ({ _southWest: sw, _northEast: ne } = map.getBounds())
+      ({ _southWest: sw, _northEast: ne } = this.map.getBounds())
       return { sw, ne }
     }
   },
@@ -48,10 +90,5 @@ Polymer({
     if (this.boundingBox) {
       this.fire('bbox', this.boundingBox)
     }
-  },
-
-  _markerClick (event) {
-    this.fire('selected', event.model.item.project._id)
   }
-
 })
