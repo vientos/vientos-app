0c8e4164b6ece848ff901e2e78cb6a3c67b61755
furenku <furenku@gmail.com>
2017-02-10 18:56:12 -0600

    fixed layout

---
+++ app/components/vientos-footer/vientos-footer.js
@@ -0,0 +1,13 @@
+Polymer({
+  is: 'vientos-footer',
+
+  actions: {
+  },
+
+  properties: {
+  },
+
+  ready () {
+  }
+
+})
---
+++ app/components/vientos-icon-button/vientos-icon-button.js
@@ -0,0 +1,21 @@
+Polymer({
+  is: 'vientos-icon-button',
+  actions: {
+  },
+
+  properties: {
+    text: {
+      type: String,
+      value: "Button Text"
+    },
+    icon: {
+      type: String,
+      value: "vientos:follow"
+    },
+  },
+
+
+  ready () {
+  }
+
+})
