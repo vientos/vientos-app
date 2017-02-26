6e1bce64fd7d7e6d7d438a58f9409049adf952c3
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-02-25 23:39:17 -0600

    badges & first test

---
+++ test/unit/reducers.js
@@ -0,0 +1,17 @@
+/* global describe, it */
+
+import { expect } from 'chai'
+
+import * as ActionTypes from '../../src/actionTypes'
+import reducers from '../../src/reducers'
+
+describe('language', function () {
+  it('state has last set language', function () {
+    let action = {
+      type: ActionTypes.SET_LANGUAGE,
+      language: 'es'
+    }
+    let state = reducers({ language: 'en' }, action)
+    expect(state.language).to.equal(action.language)
+  })
+})
