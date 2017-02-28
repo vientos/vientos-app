bebfe57d76f1ac09fedb51ac664c7178c9a49eaa
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-02-28 16:49:06 -0600

    refactored sagas

--- src/vientos.js
+++ src/vientos.js
@@ -1,50 +1,43 @@
 import fetch from 'isomorphic-fetch'
+import * as ActionTypes from './actionTypes'
 const api = require('../config.json').api
 
-export function get (action) {
-  let url = api[action.type.replace('FETCH_', '').replace('_REQUESTED', '').replace('_', '-').toLowerCase()]
-  return fetch(url, { credentials: 'include' })
-      .then(response => response.json())
-}
-
-export function bye () {
-  return fetch(api.bye, { method: 'PUT', credentials: 'include' })
-}
-
-export function follow (personId, projectId) {
-  let url = api.follow.replace('{personId}', personId).replace('{projectId}', projectId)
-  return fetch(url, { method: 'PUT', credentials: 'include' })
-}
-
-export function unfollow (personId, projectId) {
-  let url = api.unfollow.replace('{personId}', personId).replace('{projectId}', projectId)
-  return fetch(url, { method: 'DELETE', credentials: 'include' })
-}
-
-export function createIntent (intent) {
-  return fetch(api.intents, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(intent)
-  }).then(response => {
-    intent._id = response.headers.get('location').split('/')[2]
-    return intent
-  })
-}
-
-export function updateIntent (intent) {
-  return fetch(api.intents + '/' + intent._id, {
-    method: 'PUT',
-    credentials: 'include',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(intent)
-  })
-}
-
-export function deleteIntent (intentId) {
-  return fetch(api.intents + '/' + intentId, {
-    method: 'DELETE',
-    credentials: 'include'
-  })
+export default function vientos (action) {
+  let url
+  switch (action.type) {
+    case ActionTypes.BYE_REQUESTED:
+      return fetch(api.bye, { method: 'PUT', credentials: 'include' })
+    case ActionTypes.FOLLOW_REQUESTED:
+      url = api.follow.replace('{personId}', action.personId).replace('{projectId}', action.projectId)
+      return fetch(url, { method: 'PUT', credentials: 'include' })
+    case ActionTypes.UNFOLLOW_REQUESTED:
+      url = api.unfollow.replace('{personId}', action.personId).replace('{projectId}', action.projectId)
+      return fetch(url, { method: 'DELETE', credentials: 'include' })
+    case ActionTypes.CREATE_INTENT_REQUESTED:
+      return fetch(api.intents, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(action.intent)
+      }).then(response => {
+        action.intent._id = response.headers.get('location').split('/')[2]
+        return action.intent
+      })
+    case ActionTypes.UPDATE_INTENT_REQUESTED:
+      return fetch(api.intents + '/' + action.intent._id, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(action.intent)
+      })
+    case ActionTypes.DELETE_INTENT_REQUESTED:
+      return fetch(api.intents + '/' + action.intentId, {
+        method: 'DELETE',
+        credentials: 'include'
+      })
+    default:
+      url = api[action.type.replace('FETCH_', '').replace('_REQUESTED', '').replace('_', '-').toLowerCase()]
+      return fetch(url, { credentials: 'include' })
+          .then(response => response.json())
+  }
 }
