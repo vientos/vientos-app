87f9b886ac124ab4a0935d9c0fc4f47ffaa68204
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-05-02 12:54:13 -0500

    simplified project preview card + removed obj references to intents

--- app/vientos-project-preview/vientos-project-preview.js
+++ app/vientos-project-preview/vientos-project-preview.js
@@ -1,14 +1,10 @@
-/* global Polymer, ReduxBehavior, util */
+/* global Polymer, ReduxBehavior, CustomEvent, util */
 
 Polymer({
   is: 'vientos-project-preview',
   behaviors: [ ReduxBehavior, Polymer.AppLocalizeBehavior ],
 
   properties: {
-    expanded: {
-      type: Boolean,
-      value: false
-    },
     project: {
       type: Object
     },
@@ -33,12 +29,9 @@ Polymer({
 
   _checkIfFollows: util.checkIfFollows,
 
-  _expandCollaborations () {
-    this.set('expanded', !this.expanded)
-  },
-
-  _linkTo (project) {
-    return util.pathFor(project, 'project')
+  _showFullProfile () {
+    window.history.pushState({}, '', util.pathFor(this.project, 'project'))
+    window.dispatchEvent(new CustomEvent('location-changed'))
   }
 
 })
--- app/vientos-projects/vientos-projects.js
+++ app/vientos-projects/vientos-projects.js
@@ -6,27 +6,8 @@ Polymer({
 
   properties: {
     projects: {
-      type: Array,
       // filtered list passed as property
-      observer: '_addIntentsToProjects'
-    },
-    intents: {
-      type: Array,
-      statePath: 'intents',
-      observer: '_addIntentsToProjects'
-    },
-    expanded: {
-      type: Boolean,
-      value: false
-    }
-  },
-
-  _addIntentsToProjects () {
-    if (this.projects && this.intents) {
-      this.projects.forEach((project, index) => {
-        this.set(['projects', index, 'offers'], util.filterProjectOffers(project, this.intents))
-        this.set(['projects', index, 'requests'], util.filterProjectRequests(project, this.intents))
-      })
+      type: Array
     }
   }
 
