4fa5e14da486d49646aea7fb90f2ab26c210b1f1
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-05-27 23:06:34 -0500

    badge for push notifications

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ self.addEventListener('push', (event) => {
   const data = event.data ? JSON.parse(event.data.text()) : { body: 'new notification' }
   event.waitUntil(
     self.registration.showNotification('Vientos', {
+      badge: 'images/push-badge.png',
       icon: 'images/logo.png',
       body: data.body,
       data: data.iri
