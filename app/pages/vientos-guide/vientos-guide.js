7aaf105fb2261060a0eaf26308dfc1a100195f94
ralex <ralex.rdz@gmail.com>
2017-09-12 12:55:25 -0500

    guide: intro section and nav

--- app/pages/vientos-guide/vientos-guide.js
+++ app/pages/vientos-guide/vientos-guide.js
@@ -1,4 +1,4 @@
-import { ReduxMixin, util } from '../../../src/engine.js'
+import { ReduxMixin } from '../../../src/engine.js'
 
 class VientosGuide extends Polymer.mixinBehaviors(
   [Polymer.AppLocalizeBehavior],
@@ -11,9 +11,51 @@ class VientosGuide extends Polymer.mixinBehaviors(
     return {
       section: {
         type: String,
-        value: 'intro'
+        value: 'intro',
+        observer: '_sectionChanged'
+      },
+      sections: {
+        type: Array,
+        value: ['intro', 'organizations', 'intents', 'search-and-filter', 'collaborations', 'my-account']
+      },
+      showingFirstSection: {
+        type: Boolean
+      },
+      showingLastSection: {
+        type: Boolean
+      },
+      language: {
+        type: String,
+        statePath: 'language'
+      },
+      resources: {
+        type: Object,
+        statePath: 'labels'
       }
     }
+  }
+
+  _sectionChanged (section) {
+    if (section === 'intro') {
+      this.set('showingFirstSection', true)
+    } else {
+      this.set('showingFirstSection', false)
+    }
+    if (section === 'my-account') {
+      this.set('showingLastSection', true)
+    } else {
+      this.set('showingLastSection', false)
+    }
+  }
+
+  _previous () {
+    let index = this.sections.indexOf(this.section)
+    this.set('section', this.sections[index - 1])
+  }
+
+  _next () {
+    let index = this.sections.indexOf(this.section)
+    this.set('section', this.sections[index + 1])
   }
 }
 window.customElements.define(VientosGuide.is, VientosGuide)
