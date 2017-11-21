11eb421145587ffa9f3f966ed83e58b9403a0df0
ralex <ralex.rdz@gmail.com>
2017-11-21 12:28:46 -0600

    filter styles

--- app/pages/search-and-filter/search-and-filter.js
+++ app/pages/search-and-filter/search-and-filter.js
@@ -54,43 +54,13 @@ class SearchAndFilter extends Polymer.mixinBehaviors(
     this.updateStyles()
   }
 
-  _selectMyCategories () {
-    this.dispatch('updateFilteredCategories', this.person.categories)
-    this.updateStyles()
-  }
-
-  _collapseSection (e) {
-    let section = Polymer.dom(e).path.find(element => element.localName === 'section')
-    let collapser = section.getElementsByClassName('collapser')[0]
-    let ironCollapse = section.getElementsByTagName('iron-collapse')[0]
-    let icon = collapser.getElementsByTagName('iron-icon')[0]
-    if (icon.icon === 'vientos:expand-more') icon.set('icon', 'vientos:expand-less')
-    else icon.set('icon', 'vientos:expand-more')
-    ironCollapse.toggle()
-  }
-
-  _toggleCollaborationType (e) {
-    let button = Polymer.dom(e).path.find(element => element.localName === 'paper-button')
-    let collaborationType = button.getAttribute('name')
-    button.active = !button.active
-    if (this.filteredCollaborationTypes.includes(collaborationType)) {
-      this.set('filteredCollaborationTypes', this.filteredCollaborationTypes.filter(colType => colType !== collaborationType))
-    } else {
-      this.set('filteredCollaborationTypes', [...this.filteredCollaborationTypes, collaborationType])
-    }
-    this.dispatch('updateFilteredCollaborationTypes', this.filteredCollaborationTypes)
-    this.updateStyles()
+  _collaborationTypeSelectionChanged (e, detail) {
+    this.dispatch('updateFilteredCollaborationTypes', detail.value)
   }
 
   _clearCollaborationTypesFilter (e) {
     this.dispatch('updateFilteredCollaborationTypes', [])
-    let ironCollapse = Polymer.dom(e).path.find(element => element.localName === 'iron-collapse')
-    ironCollapse.querySelectorAll('vientos-icon-button').forEach(button => button.set('active', false))
     this.updateStyles()
-  }
-
-  _isCollaborationTypeSelected (collaborationType, filteredCollaborationTypes) {
-    return filteredCollaborationTypes.includes(collaborationType.id)
   }
 }
 window.customElements.define(SearchAndFilter.is, SearchAndFilter)
