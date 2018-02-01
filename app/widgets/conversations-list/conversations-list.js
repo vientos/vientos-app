dbe9849645de05cd861d769a4e53f60c50fc199d
ralex <ralex.rdz@gmail.com>
2018-02-01 16:56:44 -0600

    conversations list improvement

--- app/widgets/conversations-list/conversations-list.js
+++ app/widgets/conversations-list/conversations-list.js
@@ -29,9 +29,9 @@ class ConversationsList extends Polymer.mixinBehaviors(
       intent: {
         type: Object
       },
-      visibleConversations: {
+      visibleConversationRecords: {
         type: Array,
-        computed: '_filterConversations(person, conversations, intent, notifications, intents, reviews)'
+        computed: '_filterConversations(person, conversations, intent, notifications, intents, reviews, people)'
       },
       notifications: {
         type: Array,
@@ -44,12 +44,26 @@ class ConversationsList extends Polymer.mixinBehaviors(
       people: {
         type: Array,
         statePath: 'people'
+      },
+      language: {
+        type: String,
+        statePath: 'language'
+      },
+      resources: {
+        type: Object,
+        statePath: 'labels'
       }
     }
   }
 
-  _getName (...args) { return util.getName(...args) }
-  _getImage (...args) { return util.getImage(...args) }
+  _getThumbnailUrl (...args) { return util.getThumbnailUrl(...args) }
+
+  _getLastMessage (person, conversation, intents, us = true) {
+    return [...conversation.messages].reverse().find(message => {
+      let sameTeam = util.sameTeam(person._id, message.creator, conversation, intents)
+      return us ? sameTeam : !sameTeam
+    })
+  }
 
   _getNotificationsCount (conversation, notifications) {
     if (Array.from(arguments).includes(undefined)) return 0
@@ -58,12 +72,23 @@ class ConversationsList extends Polymer.mixinBehaviors(
     }).length
   }
 
+  _timeAgo (message) {
+    let date = message.createdAt
+    let timeDifference = Date.now() - new Date(date)
+    let oneMinute = 1000 * 60
+    let oneHour = oneMinute * 60
+    let oneDay = oneHour * 24
+    if (timeDifference < oneHour) return `${Math.floor(timeDifference / oneMinute)} ${this.localize('label:short-minute')}`
+    else if (timeDifference < oneDay) return `${Math.floor(timeDifference / oneHour)} ${this.localize('label:short-hour')}`
+    else return `${Math.floor(timeDifference / oneDay)} ${this.localize('label:short-day')}`
+  }
+
   _showConversation (e) {
     window.history.pushState({}, '', util.pathFor(e.model.conversation, 'conversation'))
     window.dispatchEvent(new CustomEvent('location-changed'))
   }
 
-  _filterConversations (person, conversations, intent, notifications, intents, reviews) {
+  _filterConversations (person, conversations, intent, notifications, intents, reviews, people) {
     if (Array.from(arguments).includes(undefined)) return []
     if (!person) return []
     return conversations.filter(conversation => {
@@ -84,6 +109,21 @@ class ConversationsList extends Polymer.mixinBehaviors(
         // TODO stars
         return new Date(b.createdAt) - new Date(a.createdAt)
       }
+    }).map(conversation => {
+      let record = {
+        notificationsCount: this._getNotificationsCount(conversation, notifications)
+      }
+      let othersLastMessage = this._getLastMessage(person, conversation, intents, false)
+      if (othersLastMessage) {
+        record.othersPerson = util.getRef(othersLastMessage.creator, people)
+        record.othersTimeAgo = this._timeAgo(othersLastMessage)
+      }
+      let usLastMessage = this._getLastMessage(person, conversation, intents, true)
+      if (usLastMessage) {
+        record.usPerson = util.getRef(usLastMessage.creator, people)
+        record.usTimeAgo = this._timeAgo(usLastMessage)
+      }
+      return record
     })
   }
 }
