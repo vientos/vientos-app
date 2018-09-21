f34a262d8c6885fb49b4f2c69c75a70ff54e1f91
Raul Rodriguez <ralex.rdz@gmail.com>
2018-09-21 15:04:39 -0500

    wip

--- test/unit/util.js
+++ test/unit/util.js
@@ -3,7 +3,7 @@ import * as util from '../../src/util'
 describe('pathFor', () => {
   test.skip('throws when parameter missing', () => {})
   let cuid = 'cff16af5-a09c-4669-8424-5eeb9afc3e79'
-  let prefix = 'https://example.org/organizations/' 
+  let prefix = 'https://example.org/organizations/'
   let iri = prefix + cuid
   let type = 'organizations'
   let path = `/${type}/${cuid}`
@@ -37,5 +37,37 @@ describe('compareIdentifiers', () => {
   })
   test('works with non matching CUID', () => {
     expect(util.compareIdentifiers('d74be7e1-5ebc-4aee-b015-adf3e5515ca3', iri)).toBeFalsy()
+  })
+})
+
+describe('getRef', () => {
+  test.skip('throws when parameter missing', () => {})
+  let prefix = 'https://example.org/places/'
+  let first = prefix + 'bc53384e-96cf-40c7-9e03-7f8d72498abc'
+  let second = prefix + '15b867d1-9c4d-420c-995c-37706ac00eb6'
+  let entityIds = [
+    first,
+    second
+  ]
+  let notExisting = prefix + '4d368e30-0e5c-456f-82df-224a8b490d39'
+  let firstMatching = { _id: first }
+  let secondMatching = { _id: second }
+  let collection = [
+    { _id: prefix + 'bbe14304-221d-438a-860a-6eb200b26fe7' },
+    { _id: prefix + 'bbcf7cf9-aca6-41a5-a8ee-d2bd319db3a8' },
+    firstMatching,
+    { _id: prefix + '5b90c7e9-c399-466d-bfb1-ad6bd3dd7f10' },
+    secondMatching
+  ]
+  let allMatching = [firstMatching, secondMatching]
+  test('works with single IRI', () => {
+    expect(util.getRef(first, collection)).toEqual(firstMatching)
+  })
+  test('works with multiple IRIs', () => {
+    expect(util.getRef(entityIds, collection)).toEqual(allMatching)
+  })
+  test('throws error entitities not in collection', () => {
+    expect(() => util.getRef(notExisting, collection)).toThrowError()
+    expect(() => util.getRef([...entityIds, notExisting], collection)).toThrowError()
   })
 })
